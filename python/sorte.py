173536f74326847ee08de6ec7ce5395c6ba28bb1
Gba704 <bielcarolinalima@gmail.com>
2025-07-16 15:28:45 -0300

    trabalahndo com progama de janelas

--- python/sorte.py
+++ python/sorte.py
@@ -1,50 +1,72 @@
 import random
 import os
 import sys
-import time 
+import time
 import webbrowser
 import tkinter as tk
 from tkinter import messagebox
 
+
 def sortear():
     opcao = 5
     numSorteado = random.randint(1, opcao)
-   # print("O número sorteado é", numSorteado)
+    #print("O número sorteado é: ", numSorteado)
 
     # try:
-    #     escolha = int(input(f"Escolha um numero entre 1 e {opcao}: "))
+    #     escolha = int(input(f"Escolha um número entre 1 e {opcao}: "))
     #     if escolha < 1 or escolha > opcao:
-    #         raise ValueError ("Número fora de intervalo! ")
+    #         raise ValueError("Número fora de intervalo! ")
+        
     # except ValueError as e:
-    #     print(f"entrada invalida: {e} tente de novo! ")
+    #     print(f"entrada inválida: {e} tente de novo!")
     #     sortear()
 
     def verificarEscolha(escolha):
         if escolha == numSorteado:
-            print("Bye Bye word, seu pc sera desligado! 🤣 ") 
+            print("Bye Bye word, seu pc será desligado!👻 ")
+            messagebox.showerror("Perdeu!", "Computador sera desligado")
             time.sleep(5)
-            if sys.platform == "wim32":
-            os.system("shutdown /s /t 1")
+            if sys.platform == "win32":
+                os.system("shutdown /s /t 1")
             elif sys.platform == 'linux' or sys.platform == 'linux2':
-            os.system("shutdown now")
-            elif sys.platform == "darwin":
-            os.system("shutdown mow")
-        else:
-            print("Você está seguro por enquanto! ")
-    janela = tk.Toplevel()
-    janela.title("Algorismo de sorteio")
-    tk.Label(janela, text="Escolha um numero de 1 a 6").pack(pady=10)
+                os.system("shutdown now")
+            elif sys.platform == "dar32 win":
+                os.system("shutdown -h now")
 
-for i in range(1,6):
-    tk.Button(janela, text=str(1), command=lambda i=i: [janela.destroy(), verificarEscolha(1)]).pack(pady=10)
+        else:
+            print("Você está seguro, por enquanto! ")
+            messagebox.showerror("Ufa!", "Você está seguro por emqunto ")
+    janela = tk.Toplevel()
+    janela.title("Algoritmo de sorteio")
+    tk.Label(janela, text="Escolha um número entre 1 e 6").pack(pady=10)
+
+    for i in range(1,6):
+        tk.Button(janela, text=str(i), command=lambda i=i: [janela.destroy(),
+        verificarEscolha(i)]).pack(pady=5)
+
+def exibriRegras():
+    regras = (
+        "Regars do jogo \n"
+        "1. Escolha um número entre 1 e 6, \n"
+        "2. Se vpcê escolher o n sorteado o seu pc sera desligado \n"
+        "3. Se não for, o jogo continua \n"
+        "4. Boa sorte, vai precisar!"
+    )
+    messagebox.showerror("Regras", regras)
+
+def sair():
+    root.destroy()
 
 
 root = tk.Tk()
-root.title("Jogo do evento aleatorio")
-tk.Label(root, text="Bem-vindo ao jogo de Evento Aleatorio", font=("Arial", 20)).pack(pady=15)
-tk.Button(root, text="iniciar jogo", width=20, command=sortear).pack(pady=10)
-tk.Button(root, text="Ver regras", width=20, command=sortear).pack(pady=10)
-tk.Button(root, text="Sair", width=20, command=sortear).pack(pady=10)
+
+root.title("Jogo do evento aleatório")
+tk.Label(root, text="Bem-vindo ao Jogo de Evento Aleatório!", font=("Arial", 20)).pack(pady=15)
+tk.Button(root, text="Iniciar Jogo", width=20, command=sortear).pack(pady=10)
+tk.Button(root, text="Ver regras", width=20, command=exibriRegras).pack(pady=10)
+tk.Button(root, text="Sair", width=20, command=sair).pack(pady=10)
+
 root.mainloop()
 
-  
+
+
---
+++ rics/script.js
@@ -0,0 +1,5 @@
+let table = new DataTable('#tabela', {
+    language: {
+        url: 'htps://cdn.datatables.net/plug-ins/2.3.2/i18n/pt-BR.json',
+    },
+});
